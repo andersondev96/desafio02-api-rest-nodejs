35ba03a39877e78ed7fad63b113916634f934fdf
Anderson Fernandes <anderson.fernandes96@outlook.com>
2023-10-18 14:42:50 -0300

    Realizar queries

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,25 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const users = await knex('users').select('*')
 
-  return tables
+  const meal = await knex('meals')
+    .insert({
+      id: crypto.randomUUID(),
+      name: 'Queijo Minas Frescal',
+      description: 'Queijo Minas Frescal Zero Gordura',
+      date: '2023-10-18',
+      time: '14:00:00',
+      isInDiet: true,
+      user_id: 'dfcf6f1c-9686-4251-92c2-62bfb9f8343d',
+    })
+    .returning('*')
+
+  return meal
 })
 
 app
